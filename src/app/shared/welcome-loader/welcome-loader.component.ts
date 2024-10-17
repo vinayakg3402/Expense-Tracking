3c9d694e2a18cdf003ccf841c70c1d2d9608131f
Vinayak Godi <138317333+vinayakg3402@users.noreply.github.com>
2024-10-17 22:25:30 +0530

    Add files via upload

---
+++ src/app/shared/welcome-loader/welcome-loader.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-loader',
+  templateUrl: './welcome-loader.component.html',
+  styleUrls: ['./welcome-loader.component.scss']
+})
+export class WelcomeLoaderComponent {
+  @Input() msg :any;
+  ngOnInit(){
+    console.log(this.msg);
+    
+  }
+}
